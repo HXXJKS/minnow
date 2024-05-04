b6e3fb279dcac115282ac5a197277851df6d7ee9
HXXJKS <hangxuan@umich.edu>
2024-05-04 16:24:59 -0400

    check 1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -18,19 +18,28 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     cout << "first index " << first_index << endl;
     cout << endl << "cur idx " << cur_idx << endl;
 
+    bool stored = false;
+    if (container_.find(first_index) != container_.end()) {
+      stored = true;
+    }
+
     // store all data
     if (first_index + data.size() <= cur_idx + avai_cap) {
       container_[first_index] = {first_index, data, is_last_substring};
-      total_stored_bytes_ += data.size();
+      //total_stored_bytes_ += data.size();
       cout << "pending 1 " << total_stored_bytes_ << endl;
     } else // otherwise modify bool
     {
       data = data.substr(0, cur_idx + avai_cap - first_index);
       container_[first_index] = {first_index, data, false};
-      total_stored_bytes_ += data.size();
+      //total_stored_bytes_ += data.size();
       cout << "pending 2 " << total_stored_bytes_ << endl;
     }
 
+    if (!stored) {
+      total_stored_bytes_ += data.size();
+    }
+    
     return;
   } 
 

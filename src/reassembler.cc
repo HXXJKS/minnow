7ff58c5d867aa7ab0d4fa24aa946abcdd60dbff5
HXXJKS <hangxuan@umich.edu>
2024-05-05 00:25:54 -0400

    check 1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -70,6 +70,18 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     } // otherwise no backmerge
     
 
+    cout << "pending 1 " << endl;
+    cout << "cur " << cur_idx << endl;
+    cout << "container info " << endl;
+    cout << "container size " << container.size() << endl;
+    auto while_ptr = container.begin();
+    while (while_ptr != container.end()) {
+      cout << "first " << while_ptr->first << " second " 
+      << while_ptr->second.first << " " << while_ptr->second.second << endl;
+      while_ptr++;
+    }
+    cout << endl;
+
     // res prev != null, forward merge
     if (res != container.begin()) {
       auto prev = std::prev(res);

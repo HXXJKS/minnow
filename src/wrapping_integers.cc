d83744c7b5656b758383797aa2bdb3a3b30c2ff7
HXXJKS <hangxuan@umich.edu>
2024-05-05 13:17:58 -0400

    check2 test

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,24 +11,16 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  
-  /*Wrap32 wraped_check = wrap(checkpoint, zero_point);
-  // right
-  uint32_t right_dis = raw_value_ - wraped_check.raw_value_;
-
-  // left
-  uint32_t left_dis = wraped_check.raw_value_ - raw_value_;
-
-  if (left_dis <= right_dis) {
-    return (checkpoint - static_cast<uint64_t>(left_dis));
-  }
-
-  return (static_cast<uint64_t>(right_dis) + checkpoint);*/
-
-  const uint64_t mod = static_cast<uint64_t>( 1 ) << 32;
+  uint64_t u32_max = static_cast<uint64_t>(1 << 32);
   uint32_t dist = raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
-  if ( dist <= ( static_cast<uint32_t>( 1 ) << 31 ) || checkpoint + dist < mod ) {
-    return dist + checkpoint;
+
+  // closer to right or before first wrap
+  if (dist < static_cast<uint64_t>(1 << 31) ||
+      dist + checkpoint < u32_max) {
+    return checkpoint + dist;
   }
-  return dist + checkpoint - mod;
+
+  // otherwise return the left one
+  return checkpoint + dist - u32_max;
+
 }

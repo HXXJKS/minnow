e84cf53fa935326b213ddf11126f2eba6dcb0e44
HXXJKS <hangxuan@umich.edu>
2024-05-05 00:47:53 -0400

    check 1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -50,7 +50,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     cout << "cur " << cur_idx << endl;
     cout << "container info " << endl;
     cout << "container size " << container.size() << endl;
-    while_ptr = container.begin();
+    auto while_ptr = container.begin();
     while (while_ptr != container.end()) {
       cout << "first " << while_ptr->first << " second " 
       << while_ptr->second.first << " " << while_ptr->second.second << endl;
@@ -89,7 +89,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     cout << "cur " << cur_idx << endl;
     cout << "container info " << endl;
     cout << "container size " << container.size() << endl;
-    auto while_ptr = container.begin();
+    while_ptr = container.begin();
     while (while_ptr != container.end()) {
       cout << "first " << while_ptr->first << " second " 
       << while_ptr->second.first << " " << while_ptr->second.second << endl;

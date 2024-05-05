929dff738ba240fa615db964f3158031d23c5d70
HXXJKS <hangxuan@umich.edu>
2024-05-05 13:18:58 -0400

    check2 test

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,11 +11,11 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  uint64_t u32_max = static_cast<uint64_t>(1 << 32);
+  uint64_t u32_max = static_cast<uint64_t>(1) << 32;
   uint32_t dist = raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
 
   // closer to right or before first wrap
-  if (dist < static_cast<uint64_t>(1 << 31) ||
+  if (dist < static_cast<uint64_t>(1) << 31 ||
       dist + checkpoint < u32_max) {
     return checkpoint + dist;
   }

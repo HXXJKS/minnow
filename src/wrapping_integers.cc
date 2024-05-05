9e21b20a14ed0919eb9f865b56a1a639d41f2003
HXXJKS <hangxuan@umich.edu>
2024-05-05 12:32:45 -0400

    check2 test

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,9 +5,8 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // static cast conversion, automaticlaly wrap around
-  Wrap32 res_raw = Wrap32(static_cast<uint32_t>(n));
-  res_raw = res_raw + zero_point;
-  return res_raw;
+  uint32_t res_raw = static_cast<uint32_t>(n);
+  return (zero_point + res_raw);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const

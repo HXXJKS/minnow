69835edf21d5234ca9ee6045752a0907b4f3b9d4
HXXJKS <hangxuan@umich.edu>
2024-05-05 15:00:45 -0400

    check2 test

--- src/reassembler.hh
+++ src/reassembler.hh
@@ -45,7 +45,7 @@ public:
   // Access output stream writer, but const-only (can't write from outside)
   const Writer& writer() const { return output_.writer(); }
 
-  uint64_t get_ackno() {return cur_idx;}
+  uint64_t get_ackno() const {return cur_idx;}
 
 private:
   ByteStream output_; // the Reassembler writes to this ByteStream

6d25ba91baf8532ea7f15b112b530a226df0606d
HXXJKS <hangxuan@umich.edu>
2024-05-05 12:47:22 -0400

    check2 test

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,8 +11,8 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  Wrap32 wraped_check = wrap(checkpoint);
+  uint32_t dis_check_this = this.raw_value_ - wraped_check.raw_value_;
+  uint64_t converted_dis = static_cast<uint64_t>(dis_check_this);
+  return (checkpoint + converted_dis);
 }
